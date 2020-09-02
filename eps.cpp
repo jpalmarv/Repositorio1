25322e325f45eb7cb0e9ae5a8da43018b84f040a
jpalmarv <jpalmarv@unal.edu.co>
2020-09-02 12:29:02 -0500

    Clase 3

--- eps.cpp
+++ eps.cpp
@@ -1,45 +1,21 @@
      #include <iostream>
-     #include <cmath>
+     #include <cstdlib>
 
-     int factorial(int n);
-     double fnaive(double x, int N);
-     double fsmart(double x, int N);
+//typedef long double REAL;
 
-     int main(void)
+     int main(int argc, char **argv)
      {
-       std::cout.precision(16); std::cout.setf(std::ios::scientific);
-       double x = 1.234534534;
-       for (int NMAX = 0; NMAX <= 100; ++NMAX) {
-         std::cout << NMAX
-                   << "\t" << fsmart(x, NMAX)
-                   << "\t" << std::fabs(fsmart(x, NMAX) - std::exp(-x))/std::exp(-x)
-                   << std::endl;
+       std::cout.precision(7);
+       std::cout.setf(std::ios::scientific);
+       
+       int N = std::atoi(argv[1]);
+       float eps = 1.0, one = 1.0;
+       for (int ii = 0; ii < N; ++ii){
+         eps /= 2.0;
+         one = 1.0 + eps;
+         std::cout << ii << "\t"
+                   << one << "\t"
+                   << eps << "\n";
        }
        return 0;
      }
-
-     double fnaive(double x, int N)
-     {
-       double term = 0, suma = 0;
-       for(int k = 0; k <= N; ++k){
-         term = std::pow(-x, k)/factorial(k);
-         suma += term;
-       }
-       return suma;
-     }
-
-     int factorial(int n)
-     {
-       if (n <= 0) return 1;
-       return n*factorial(n-1);
-     }
-
-     double fsmart(double x, int N)
-     {
-       double term = 1, suma = 1;
-       for(int k = 0; k < N; ++k){
-         term *= (-x)/(k+1);
-         suma += term;
-       }
-       return suma;
-   }
