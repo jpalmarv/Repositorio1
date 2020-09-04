a30f7e7968a051c98725c0bf03f4385232f9b2a5
Jorge Palmar <jpalmarv@unal.edu.co>
2020-09-04 14:33:49 -0500

    Final, Error misma funcion dos formas

--- Tarea1,1.cpp
+++ Tarea1,1.cpp
@@ -5,19 +5,18 @@
 float original(float x);
 float mejor(float x);
 
-
-
 int main(void)
 {
   std::cout.precision(8); std::cout.setf(std::ios::scientific);
  
   std::cout << "Grupo:\n";
-  std::cout << "Jorge Palmar Velasco\n";
+  std::cout << "Sergio Lopez\n";
   std::cout << "Jorge Palmar Velasco\n";
 
   float a = 0.01;
   
-  for(int  ii = 0; ii <= 100; ++ii){
+  for(int  ii = 0; ii <= 100; ++ii)
+  {
     
     float x1 = ii * a;
     
@@ -31,7 +30,7 @@ int main(void)
 
 float original(float x)
 {
-  x =  5 - std::sqrt( 25 + std::pow(x,2));
+  x =  5 - std::sqrt( 25 + x*x);
   return x;
 }
 float mejor(float x)
