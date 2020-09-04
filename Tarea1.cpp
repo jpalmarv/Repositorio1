0076478c42bafe33c99a3fca537a222b9c82d339
jpalmarv <jpalmarv@unal.edu.co>
2020-09-03 23:59:54 -0500

    Con funcion ,mejor intento 1

--- Tarea1.cpp
+++ Tarea1.cpp
@@ -4,13 +4,14 @@
      
 float original(float x);
 float mejor(float x);
+float mejor2(float x);
 
 int main(void)
 {
   std::cout.precision(8); std::cout.setf(std::ios::scientific);
 
-  for(float ii = 0; ii <= 1; ii = ii + 0.01){
-    std::cout << ii << "\t" << original(ii) << "\n";
+  for(float ii = 0.00; ii <= 1.00; ii = ii + 0.01){
+    std::cout << ii << "\t" << original(ii) << "\t" << mejor2(ii) << "\n";
 	      
   }
   return 0;
@@ -18,6 +19,19 @@ int main(void)
 
 float original(float x)
 {
-  x = 5 - std::sqrt( 25 - std::pow(x,2));
+  x = 5 - std::sqrt( 25 + std::pow(x,2));
   return x;
 }
+
+float mejor(float x)
+{
+  x = 5 - std::sqrt( std::pow((x+5),2)-10*x);
+  return x;
+}
+
+float mejor2(float x)
+{
+  x = -(std::sqrt( 25 + std::pow(x,2)) - 5);
+  return x;
+}
+  
