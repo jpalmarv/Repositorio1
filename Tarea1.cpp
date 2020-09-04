56bedd5ddbf0b0905214c5339cef9a68dc56da48
jpalmarv <jpalmarv@unal.edu.co>
2020-09-03 20:51:12 -0500

    Tarea 1, programa beta 1, solo original

---
+++ Tarea1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+     
+float original(float x);
+float mejor(float x);
+
+int main(void)
+{
+  std::cout.precision(8); std::cout.setf(std::ios::scientific);
+
+  for(float ii = 0; ii <= 1; ii = ii + 0.01){
+    std::cout << ii
+	      << "\t" << original(ii)
+	      << std::endl;
+  }
+  return 0;
+}
+
+float original(float x)
+{
+  x = 5 - std::sqrt( 25 - std::pow(x,2));
+  return x;
+}
