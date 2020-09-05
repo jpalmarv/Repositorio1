cc5c40c08ca3cf5dbe29522b4bc75fc8e895a316
Jorge Palmar <jpalmarv@unal.edu.co>
2020-09-05 12:15:54 -0500

    Final Tarea 1

--- Tarea1,1.cpp
+++ Tarea1,1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cmath>
-
+//Se quiere calcular f(x)=5-sqrt(25+x²) para x=0 hasta 1 con pasos de 0.01.
+//Se trata de encontrar una forma que evite el error de resta.
+//Se encuentra la forma alternativa de la funcón f(x)=-x²/(5+sqrt(25+x²)).
 
 float original(float x);
 float mejor(float x);
@@ -9,32 +11,30 @@ int main(void)
 {
   std::cout.precision(8); std::cout.setf(std::ios::scientific);
  
-  std::cout << "Grupo:\n";
-  std::cout << "Sergio Lopez\n";
-  std::cout << "Jorge Palmar Velasco\n";
+  std::cout << "Grupo: Sergio Lopez, Jorge Palmar.\n\n";
 
-  float a = 0.01;
-  
-  for(int  ii = 0; ii <= 100; ++ii)
+  for(int ii = 0; ii <= 100; ++ii)
   {
-    
-    float x1 = ii * a;
-    
+    float x1 = ii * 0.01;
+   
     std::cout << x1
-	      << "\t" << original(x1)
-	      << "\t" << mejor(x1)
-	      << "\n";    
-  }
+	            << "\t" << original(x1)
+	            << "\t" << mejor(x1)
+              << "\n";
+	 }
   return 0;
 }
-
+//Se implementa f(x)=5-sqrt(25+x²).
 float original(float x)
 {
-  x =  5 - std::sqrt( 25 + x*x);
-  return x;
+  float Fun;
+  Fun =  5 - std::sqrt(25 + x*x);
+  return Fun;
 }
+//Se implementa f(x)=-x²/(5+sqrt(25+x²)).
 float mejor(float x)
 {
-  x = -1.0*x*x/( 5 + std::sqrt( 25 + x*x));
-  return x;
+  float Fun;
+  Fun = -x*x/( 5 + std::sqrt(25 + x*x));
+  return Fun;
 }
