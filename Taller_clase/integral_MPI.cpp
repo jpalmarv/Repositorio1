e4c2922c3f42ded779072251c4c21411bb953729
Jorge Palmar <jpalmarv@unal.edu.co>
2020-11-20 12:12:05 -0500

    taller en clase, algunas correcciones

--- Taller_clase/integral_MPI.cpp
+++ Taller_clase/integral_MPI.cpp
@@ -30,19 +30,20 @@ int main(int argc, char **argv)
     return 0;
 }
 
-
 void integral(int ns,  int nslocal, int pid, int nproc)
 {
     int tag = 0;
-    double x = 0.0, dx = 0.0, inlocal = 0.0;
+    double x = 0.0, dx = 0.0, inlocal = 0.0, sum = 0.0;
     dx = 10.0/ns;
     for(int ilocal = 0; ilocal < nslocal; ++ilocal){
         x = (ilocal+pid*nslocal)*dx;
         inlocal += x*x*dx;
     }
     if(pid == 0){
-        double aux, sum;
+        double aux
+            ;
         sum = inlocal;
+        
         for(int src = 1; src < nproc; ++src){
             MPI_Recv(&aux, 1, MPI_DOUBLE, src, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             sum += aux;
