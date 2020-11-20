d518eede709fa73b8d5efacb2d8066f9fb4b1f50
Jorge Palmar <jpalmarv@unal.edu.co>
2020-11-20 13:45:45 -0500

    Correcciones

--- Taller_clase/scaling1.sh
+++ Taller_clase/scaling1.sh
@@ -1,4 +1,4 @@
-for NTH in 1 2 3 4 5 6 7 8; do
+for NTH in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16; do
         echo -n "$NTH  ";
          mpirun -oversubscribe  -np $NTH ./a.out;
     done > scaling.txt
